7ed33768bf1349feef58a83b6a076e3f327de0be
Irakli Gozalishvili <rfobic@gmail.com>
2011-07-25 15:35:33 +0200

    Initial commit.

---
+++ Readme.md
@@ -0,0 +1,11 @@
+# functional #
+
+functional javascript <3
+
+## Install ##
+
+    npm install functional
+
+## Require ##
+
+    var functional = require('https!raw.github.com/Gozala/functional/v0.0.1/functional.js')
---
+++ functional.js
@@ -0,0 +1,11 @@
+/* vim:set ts=2 sw=2 sts=2 expandtab */
+/*jshint asi: true undef: true es5: true node: true devel: true
+         forin: true latedef: false supernew: true */
+/*global define: true */
+
+(typeof define === "undefined" ? function ($) { $(require, exports, module); } : define)(function (require, exports, module, undefined) {
+
+"use strict";
+
+
+})
